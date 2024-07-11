ce17923e7f287e470adeb98bb276d0c0f7b1c5b6
Tmrbot4 <166976733+Tmrbot4@users.noreply.github.com>
2024-07-11 11:17:13 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -32,9 +32,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN', 'https://filesstore-6482ba8ae422.herokuapp.com/') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL', True))
     if HAS_SSL:
-        URL = "https://{}/".format(FQDN)
+        URL = "https://filesstore-6482ba8ae422.herokuapp.com/".format(FQDN)
     else:
-        URL = "http://{}/".format(FQDN)
+        URL = "https://filesstore-6482ba8ae422.herokuapp.com/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://tabolo8539:0evqZDV4fC5fD17c@cluster0.cw8zxus.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'the_movie_rock'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))
